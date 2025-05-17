5abe8946455db1b22fdce57b93ec9cd0f426f767
Navnath Kadam <navnathkadam284@gmail.com>
2025-05-17 11:22:52 +0530

    Update namespaceAdv.cpp

--- Namespace/namespaceAdv.cpp
+++ Namespace/namespaceAdv.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 // we can add multiple myspace
 // we can't write namespace inside function
-// when we declare multiple namespace with same name then I c++ allowed that and add into that namespace as u can see in below example 
+// when we declare multiple namespace with same name then I c++ allowed that and add into that namespace as u can see in below example
+// this also allowed in multiple file 
+
+// that's how std also work like
+/*
+   if we write iostream header then inside iostream header there is std namespace that adds cout , cin function to the std 
+
+  again if we write string header then inside string header there is std namespace that adds string related function 
+
+ so then we are able to use cout , string and all after writing header 
+*/
 
    namespace myspace {
        int value =10;
